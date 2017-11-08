e05ee29024ece3367768d8c13ac2e6d7fc8e4b30
Abu Jafar Md. Salah <naharsoftbd@gmail.com>
2017-11-08 07:19:31 +0600

    app Directory uploaded

---
+++ auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Router, RouterModule } from "@angular/router";
+
+export class User {
+  constructor(
+    public email: string,
+    public password: string) { }
+}
+ 
+var users = [
+  new User('jafar@utshob.com','123456'),
+  new User('user1@gmail.com','a23')
+];
+
+@Injectable()
+export class AuthService {
+   redirectUrl: string;
+  constructor(
+    private _router: Router){}
+ 
+  logout() {
+    localStorage.removeItem("user");
+    this._router.navigate(['/login']);
+  }
+ 
+  login(user){
+    var authenticatedUser = users.find(u => u.email === user.email);
+    if (authenticatedUser && authenticatedUser.password === user.password){
+      localStorage.setItem("user", authenticatedUser.email);
+      this._router.navigate(['/home']);      
+      return true;
+    }
+    return false;
+ 
+  }
+ 
+   checkCredentials(){
+    if (localStorage.getItem("user") === null){
+        this._router.navigate(['/login']);
+    }else{
+       this._router.navigate(['/home']); 
+    }
+  } 
+
+}
